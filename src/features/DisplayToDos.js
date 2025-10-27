7dd456e0c1f177aaa7594518700613c34c3087d8
D0ts_fav_dev <Corevirus1738@gmail.com>
2025-10-26 20:55:22 -0700

    Clears users input upon successful change

--- src/features/DisplayToDos.js
+++ src/features/DisplayToDos.js
@@ -1,22 +1,25 @@
+import { useState } from "react";
 
 export default function DisplayToDos({toDos, setToDos, userInput, setUserInput}) {
+   
     function removeToDo(index) {
         setToDos(prevArray => 
             prevArray.filter((_, slot) => slot !== index)
         )
-
     }
-            function editToDo(index) {
-            setToDos(prevArray => {
-                if (!prevArray.includes(userInput)) {
-                    return prevArray.map((item, i) => i === index ? userInput : item)
-                } else {
-                    alert("This item already exists!");
-                    return prevArray
-                }
+    function editToDo(index) {
+        setToDos(prevArray => {
+            if (!prevArray.includes(userInput)) {
+                const newArray = prevArray.map((item, i) => i === index ? userInput : item)
+                setUserInput('')
+                return newArray
+            } else {
+                alert("This item already exists!");
+                return prevArray
             }
-            )
-        }
+                }
+                    )
+                        }
 
     return (
         <>
