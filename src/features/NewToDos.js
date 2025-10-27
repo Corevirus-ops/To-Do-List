9eb834bbfacd5b2b5edbf84845c13801455f6c2b
D0ts_fav_dev <Corevirus1738@gmail.com>
2025-10-27 01:00:08 -0700

    added a max char limit to protect against extreme cases

--- src/features/NewToDos.js
+++ src/features/NewToDos.js
@@ -2,10 +2,15 @@
 
 export default function NewToDos({userInput, setUserInput}) {
 
+    function setInput(target) {
+        if (target.length > 20) return
+        setUserInput(target)
+    }
+
     return (
         <div className="userInput">
         <label htmlFor="userInput">New To Do</label>
-        <input id="userInput" type="text" min={1} value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+        <input id="userInput" type="text" value={userInput} onChange={(e) => setInput(e.target.value)} />
         </div>
     )
 
